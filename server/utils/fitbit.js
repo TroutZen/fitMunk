a04d0f3325eb10ece7675a4d564107f68d304331
Devin Otway <devin.otway@gmail.com>
2015-02-06 12:14:31 -0800

    sever/fetches fitbit data signed in user at one date

--- server/utils/fitbit.js
+++ server/utils/fitbit.js
@@ -9,28 +9,38 @@ module.exports = exports = {
       consumerSecret: process.env.CONSUMER_SECRET,
       callbackURL: '/auth/fitbit/callback',
       userAuthorizationURL: 'https://www.fitbit.com/oauth/authorize'
-    }, function (token, tokenSecret, profile, done) {   
+    }, function (token, tokenSecret, profile, done) {
+
+      console.log('Devins Profile Id', profile.id);
+       
       //after oath login call this success handler
           //add user to db
           Users.addUser(token, tokenSecret, profile, function(d){
             //this line waits for 26 to finish
-            exports.getStats(profile.id, token, tokenSecret).then(function() { 
-              //done tells the program you are done and want to go to the next step
-              console.log(d)
-              done(null, d); 
-            });
+            exports.getStats(token, tokenSecret).then(function(){
+              done(null, d);
+            }); 
+            // exports.getStats(profile.id, token, tokenSecret).then(function() { 
+            //   //done tells the program you are done and want to go to the next step
+            //   console.log(d)
+            //   done(null, d); 
+            // });
           })
 
         }),
 
-  getStats: function (userID, token, secret) {
+  getStats: function (token, tokenSecret) {
+    console.log('Attempting to get fitbit stats');
     var client = new FitbitApiClient(process.env.CONSUMER_KEY, process.env.CONSUMER_SECRET);
+    var DevinsUserId = '36SQBB';
     //creates the request to get activites json from fitbit
-    return client.requestResource('/activities.json', 'GET', token, secret).then(function (data) {  
+    // '/activities/steps/date/2015-02-01/2015-02-05/.json'
+    return client.requestResource('/activities/date/2015-02-04.json', 'GET', token, tokenSecret, DevinsUserId).then(function (data) {  
         //success handler for req, return the promise
-        Users.addUserStats(userID,data[0]);
+        console.log('fitbit data', data);
+        // Users.addUserStats(userID,data[0]);
       }, function (err) {
-        console.log('ERROR!',err);
+        console.log('[Error] Attempting to fetch steps data from fitbit API', err);
       });
   }
 
